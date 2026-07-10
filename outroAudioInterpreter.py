a2fed7b4aabfa6c06a3a062e5bebd1d43c59655c
Carzon <justlooking960@gmail.com>
2026-07-09 22:34:47 -0400

    Update outroAudioInterpreter.py

--- outroAudioInterpreter.py
+++ outroAudioInterpreter.py
@@ -2,12 +2,12 @@ import ffmpeg
 import ffmpeg.audio
 
 def audioIsForEnding(sensitivity, gameAudio, sampleAudio):
-    
     overlayedAudio = ffmpeg.filter([gameAudio,sampleAudio], 'amix', duration='shortest')
     if(overlayedAudio.volume < sensitivity):
         return True
     else:
         return False
+    
 def test(sensitivity):
     sample = ffmpeg.audio("./audioSample.mp3")
     overlayedAudio = ffmpeg.filter([sample,sample], 'amix', duration='shortest')
