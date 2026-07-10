286eabec382e42b6541843b0700be239cecf616f
Carzon <justlooking960@gmail.com>
2026-07-09 23:22:14 -0400

    Update main.py
    Simple code to open application.

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import ffmpeg
 import livesplitInterface
 import outroAudioInterpreter
+from ui import PASAWindow
 
-sample = ffmpeg.audio("./audioSample.mp3")
+"""sample = ffmpeg.audio("./audioSample.mp3")"""
+
+def main():
+    app = PASAWindow()
+    app.run()
+
+
+if __name__ == "__main__":
+    main()
